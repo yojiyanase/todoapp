30ea84f7c4219444490b866ef4a0c615c0a14e62
yanase <tennisman_225@yahoo.co.jp>
2023-08-26 11:49:28 +0900

    Boardの投稿部分の作成

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,9 +1,34 @@
 class BoardsController < ApplicationController
     def index
+        @boards = Board.all
+    end
 
+    def show
+        @board = current_user.boards.find(params[:id])
     end
     
     def new
-        @board = Board.new
+        @board = current_user.boards.build
+    end
+
+    def create
+        @board = current_user.boards.build(board_params)
+        if @board.save
+        redirect_to board_path(@board), notice: '保存できたよ'
+        else
+        flash.now[:error] = '保存に失敗しました'
+        render :new
+        end
+    end
+
+
+    private
+
+    def board_params
+        params.require(:board).permit(:title, :description)
+    end
+
+    def set_board
+        @board = Board.find(params[:id])
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :boards, dependent: :destroy
+
+  def has_written?(board)
+    boards.exists?(id: board.id)
+  end
+
+  def display_name
+    # profile&.nickname || self.email.split('@').first
+  end
+
 end
