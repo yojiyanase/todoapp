c55da0ce54a0de95c02ea7050e7f85cedb13f8fa
yanase <tennisman_225@yahoo.co.jp>
2023-08-26 11:44:37 +0900

    Boardモデルのカラム名の変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_24_132232) do
+ActiveRecord::Schema.define(version: 2023_08_25_133635) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "boards", force: :cascade do |t|
-    t.string "name"
+    t.string "title"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
