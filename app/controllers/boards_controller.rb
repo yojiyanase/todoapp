4e112a8ae6a4867f3daaba616ad9fa6f61cbe095
yanase <tennisman_225@yahoo.co.jp>
2023-08-27 17:50:26 +0900

    Boardのeditページの作成

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,4 +1,7 @@
 class BoardsController < ApplicationController
+    before_action :set_board, only: %i[show]
+    before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+
     def index
         @boards = Board.all
     end
@@ -21,11 +24,30 @@ class BoardsController < ApplicationController
         end
     end
 
-
+    def edit
+        @board = current_user.boards.find(params[:id])
+    end
+    
+    def update
+        @board = current_user.boards.find(params[:id])
+        if @board.update(board_params)
+          redirect_to board_path(@board), notice: '更新できました'
+        else
+          flash.now[:error] = '更新できませんでした'
+          render :edit
+        end
+    end
+    
+    def destroy
+        board = current_user.boards.find(params[:id])
+        board.destroy!
+        redirect_to root_path, notice: '削除に成功しました'
+    end
+    
     private
 
     def board_params
-        params.require(:board).permit(:title, :description)
+        params.require(:board).permit(:title, :content)
     end
 
     def set_board
