18b040e9d4c1144cb2c2759d4a07b23f038593ef
yanase <tennisman_225@yahoo.co.jp>
2023-08-27 17:57:36 +0900

    バリデーションとエラーの表示

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,5 +1,13 @@
 class Board < ApplicationRecord
 
+  validates :title, presence: true
+  validates :title, length: { minimum: 2, maximum: 100 }
+  validates :title, format: { with: /\A(?!@)/ }
+
+  validates :content, presence: true
+  validates :content, length: { minimum: 10 }
+  validates :content, uniqueness: true
+
     belongs_to :user
 
     def display_created_at
