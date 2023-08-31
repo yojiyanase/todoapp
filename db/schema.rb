cbe7f37743a648db414b4e000dffccdce114ffd4
yanase <tennisman_225@yahoo.co.jp>
2023-08-31 21:54:48 +0900

    userとtaskの紐づけ

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_29_130320) do
+ActiveRecord::Schema.define(version: 2023_08_30_130749) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(version: 2023_08_29_130320) do
     t.date "limit"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
