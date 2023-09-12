fb4ace8414e4e760396655892a47f9fd9835972c
yanase <tennisman_225@yahoo.co.jp>
2023-09-12 10:11:28 +0900

    Boardの下にTaskとCommentをぶら下げ、BoardのShowページのリンク修正

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -8,7 +8,7 @@ class BoardsController < ApplicationController
 
     def show
         @board = Board.find(params[:id])
-        @tasks = Task.all
+        @tasks = @board.tasks
     end
     
     def new
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "boards#index"
 
-  resources :boards
-  resources :tasks
+  resources :boards do
+    resources :tasks do
+      resources :comments, only: [:new, :create]
+    end
+  end
 
   resource :profile, only: [:show, :edit, :update]
 end
