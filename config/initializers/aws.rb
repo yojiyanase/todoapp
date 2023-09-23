1472d35168c5d9f3f531a2b0723788a9ddfb1306
yanase <tennisman_225@yahoo.co.jp>
2023-09-23 12:10:42 +0900

    aws.rbの削除

--- config/initializers/aws.rb
+++
@@ -1,6 +0,0 @@
-s3 = Aws::S3::Resource.new(
-      credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'],  ENV['AWS_SECRET_ACCESS_KEY']),
-      region: 'us-west-1'
-    )
-
-    obj = s3.bucket(ENV['S3_BUCKET']).object('key')
